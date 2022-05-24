394a8d6d5c1d0703a12cc98ee142f4b53f2849e1
alexkarunny <smetan4ik1@gmail.com>
2022-05-24 16:44:35 +0300

    11. Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/images/logo.png';
+import fon from './assets/images/fon.jpg'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className={'app_header'}>
+          <img src={logo} alt="" className={'logo'}/>
       </header>
+      <nav className={'app_navbar'}>
+          <div><a href="">Profile</a></div>
+          <div><a href="">Messages</a></div>
+          <div><a href="">News</a></div>
+          <div><a href="">Music</a></div>
+          <div><a href="">Settings</a></div>
+      </nav>
+      <div className={'app_content'}>
+        <div>
+            <img src={fon} alt="" className={'fon'}/>
+        </div>
+          <div>ava + description</div>
+          <div>
+              My posts
+              <div>
+                  new post
+              </div>
+              <div>post 1</div>
+              <div>post 2</div>
+          </div>
+      </div>
+
     </div>
   );
 }
